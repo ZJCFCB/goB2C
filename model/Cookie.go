e0347263ed17798e9e404bd3a958cec226d005b4
ZJCFCB <15674683467@163.com>
2024-05-23 12:58:34 +0800

    新增cookie的相关内容

--- model/Cookie.go
+++ model/Cookie.go
@@ -1,10 +1,34 @@
 package model
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
 type cookie struct{}
 
-/*
+var Cookie = &cookie{}
+
 func (c cookie) Set(ctx *gin.Context, key string, value interface{}) {
 	bytes, _ := json.Marshal(value)
-	ctx.SetSecureCookie(viper.GetString("secureCookie"), key, string(bytes), 3600*24*30, "/", viper.GetString("domain"), nil, true)
+	ctx.SetCookie(key, string(bytes), 3600*60*7, "/", viper.GetString("domain"), true, true)
 }
-*/
+
+func (c cookie) Remove(ctx *gin.Context, key string, value interface{}) {
+	bytes, _ := json.Marshal(value)
+	ctx.SetCookie(key, string(bytes), -1, "/", viper.GetString("domain"), true, true)
+}
+func (c cookie) Get(ctx *gin.Context, key string, obj interface{}) bool {
+	temp, ok := ctx.Cookie(key)
+	if ok != nil {
+		return false
+	}
+	if err := json.Unmarshal([]byte(temp), obj); err != nil {
+		fmt.Println(err)
+		return false
+	}
+	return true
+}
